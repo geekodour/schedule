12b2a8fadd7e7508eb488be210b3a6f8ca73d937
Hrishikesh Barman <oss@geekodour.org>
2024-05-09 23:15:59 +0530

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,18 @@
 <svelte:head>
-	<title>geekodour's streams</title>
+  <title>geekodour's streams</title>
 </svelte:head>
 
-<div class="flex justify-center gap-2 px-4">
-<div>
-<h1 class="text-4xl font-spice">geekodour's public streams</h1>
-<h2 class="text-2xl">
-i stream bad art, bad guitar practice and bad programming practice
-</h2>
-<div class="mt-4">
-<a href="https://www.twitch.tv/geekodour08" class="text-2xl underline">
-twitch/geekodour08
-</a>
-</div>
-</div>
-<div>
-<iframe src="https://calendar.google.com/calendar/embed?src=dd6da266c6a02a9ac71d91aad02821c3cc174ff9e9d8f8dfab03da0c1d97c736%40group.calendar.google.com&ctz=Asia%2FKolkata" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-</div>
+<div class="flex flex-col px-4">
+  <div class="flex flex-col items-center">
+    <h1 class="text-4xl font-spice">geekodour's public streams</h1>
+    <h2 class="text-2xl"> i stream bad art, bad guitar practice and bad programming practice </h2>
+    <div class="mt-4">
+      <a href="https://www.twitch.tv/geekodour08" class="text-2xl underline">
+        twitch/geekodour08
+      </a>
+    </div>
+  </div>
+  <div class="flex justify-center">
+    <iframe src="https://calendar.google.com/calendar/embed?src=dd6da266c6a02a9ac71d91aad02821c3cc174ff9e9d8f8dfab03da0c1d97c736%40group.calendar.google.com&ctz=Asia%2FKolkata" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+  </div>
 </div>
